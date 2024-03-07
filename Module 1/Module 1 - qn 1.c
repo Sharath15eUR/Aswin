28909097b20982b80265c540b90174c2477406ae
aswinsankesh <138267592+aswinsankesh@users.noreply.github.com>
2024-03-07 21:49:34 +0530

    Add files via upload

---
+++ Module 1/Module 1 - qn 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if (num & 1) {
+        printf("%d is odd number", num);
+    } else {
+        printf("%d is even number", num);
+    }
+    
+    return 0;
+}
